cf28eda4e914b3410c17997c64084fe90cdc010b
Fennec120 <132257535+Fennec120@users.noreply.github.com>
2023-07-01 16:07:31 +0300

    first commit

--- area_calculation_lib/area_calculation_lib.py
+++ area_calculation_lib/area_calculation_lib.py
@@ -1,18 +1,19 @@
-import math
-
-
+#base class
 class area_calculator:
     def __init__(self):
         pass
 
+    #providing triangle sides' lengths
     def triangle_set_params(self, side_1, side_2, side_3):
         self.side_1 = side_1
         self.side_2 = side_2
         self.side_3 = side_3
 
+    # providing round radius' length
     def round_set_params(self, rad):
         self.rad = rad
 
+    #triangle area calculation w\ additional checking for right angle
     def triangle_area(self):
         import math
 
@@ -27,6 +28,7 @@ class area_calculator:
 
         return math.sqrt(pp * (pp - self.side_1) * (pp - self.side_2) * (pp - self.side_3))
 
+    #round area calculation
     def round_area(self):
         print(self.rad ** 2 * 3.14)
         return self.rad ** 2 * 3.14
--- area_calculation_lib/test.py
+++ area_calculation_lib/test.py
@@ -1,13 +1,16 @@
 import unittest
+from area_calculation_lib import area_calculator as ac
 
 
 class TestTriangleArea(unittest.TestCase):
     def test_triangle_area(self):
         from area_calculation_lib import area_calculator as ac
         self.triangle = ac()
-        self.triangle.triangle_set_params(5,4,3)
+        self.triangle.triangle_set_params(5, 4, 3)
         result = self.triangle.triangle_area()
         self.assertEqual(result, 6)
 
 
-
+my_round = ac()
+my_round.round_set_params(6)
+my_round.round_area()
